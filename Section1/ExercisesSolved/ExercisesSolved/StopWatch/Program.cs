d98da149d8e9d829cc64a28e2529fdc95de6c16b
drinoff <drinoff@gmail.com>
2020-09-26 17:27:56 +0200

    StopWatch needs some Work

---
+++ Section1/ExercisesSolved/ExercisesSolved/StopWatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            DateTime startTime;
+            while (true)
+            {
+                Console.WriteLine("Please press \"Y\" to start the StopWatch");
+
+                var userInput = Console.ReadKey();
+                var pressedKey = userInput.KeyChar.ToString();
+                if(pressedKey == "Y")
+                {
+                     startTime = stopWatch.Start();
+                }
+                Console.WriteLine("Please Press \"N\" to stop the StopWatch");
+                userInput = Console.ReadKey();
+                pressedKey = userInput.KeyChar.ToString();
+                if(pressedKey == "N")
+                {
+                    stopWatch.Stop(startTime);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Section1/ExercisesSolved/ExercisesSolved/StopWatch/StopWatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace StopWatch
+{
+   public class StopWatch
+    {
+        public bool isStarted { get; private set; }
+        public TimeSpan duration { get; private set; }
+
+        public  DateTime Start()
+        {
+            this.duration = TimeSpan.Zero; 
+            if (this.isStarted == true)
+            {
+               throw new InvalidOperationException(message: "StopWatch is alread Started");
+            }
+            this.isStarted = true;
+            return DateTime.Now;
+            
+        }
+
+        public  TimeSpan Stop(DateTime startTime)
+        {
+            if(this.isStarted == false)
+            {
+                throw new InvalidOperationException(message: "StopWatch is already Stopped");
+            }
+            this.isStarted = false;
+            return this.duration = DateTime.Now - startTime;
+            
+        }
+        public static void Exit()
+        {
+            System.Environment.Exit(0);
+        }
+        public  bool  Status()
+        {
+            return this.isStarted;
+        }
+    }
+}
